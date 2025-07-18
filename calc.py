bd826be2c63d7ef74972acee18157e2fad8b122d
lu1sm91 <lu1sm4rt1n91@icloud.com>
2025-07-18 14:22:19 -0500

    Refactored calculator with separate operations module

--- calc.py
+++ calc.py
@@ -1,26 +1,20 @@
-def add(x, y):
-    return x + y
-def subtract(x, y):
-    return x - y
-def multiply(x, y):
-    return x * y
-def divide(x, y):
-    if y == 0:
-        return "Cannot divide by zero"
-    return x / y
+from operations import add, subtract, multiply, divide #This import my functions
+
 def main():
     print("Simple Calculator")
-    x = int(input("Enter first number: "))
-    y = int(input("Enter second number: "))
+    x = float(input("Enter first number: "))
+    y = float(input("Enter second number: "))
     op = input("Enter operation (+, -, *, /): ")
-    
-    if op == "+":
-        print("Result:", add(x, y))
-    elif op == "-":
-        print("Result:", subtract(x, y))
-    elif op == "*":
-        print("Result:", multiply(x, y))
-    elif op == "/":
-        print("Result:", divide(x, y))
+
+    if op == '+':
+        print ("result = ", add(x, y))
+    elif op == '-':
+        print(f"result = {subtract(x, y)}")
+    elif op == '*':
+        print("reult = ", multiply(x, y))
+    elif op == '/':
+        print(f"result = {divide(x, y)}")
     else:
-        print("Invalid operation.") 
+        print ("Invalid operation")
+if __name__ == "__main__":
+    main()
