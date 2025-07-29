c816ac0d99e0b4761c348492ea1f2ef1bc928fd0
lu1sm91 <lu1sm4rt1n91@icloud.com>
2025-07-29 16:38:23 -0500

    Day 2 practice- string formatting

---
+++ day2_variables.py
@@ -0,0 +1,6 @@
+first_name = input(f'Enter your first name: ')
+last_name = input(f'Enter your last name: ')
+
+fullname = print(f'{first_name} {last_name}') 
+
+#Dont know how to print in upper, lower and title case.
