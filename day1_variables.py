1c7c2c37e9a9c21a3f27ccd35a695727dbde1cb5
lu1sm91 <lu1sm4rt1n91@icloud.com>
2025-07-29 16:19:12 -0500

    Fix print function call

--- day1_variables.py
+++ day1_variables.py
@@ -1,5 +1,5 @@
-name = input(f'What is your name?')
-color = input('fWhats your favorite color?')
-age = int(input(f'How old are you?'))
+name = input(f'What is your name? ')
+color = input(f'Whats your favorite color? ')
+age = int(input(f'How old are you?' ))
 
-print = (f'Hello {name}! Your favorite color is {color} and you are {age} years old.)
+print  (f'Hello {name}! Your favorite color is {color} and you are {age} years old.')
