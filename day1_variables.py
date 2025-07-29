5d7d4d8a5813c4e4c86bf89aa9df161e3fd7f776
lu1sm91 <lu1sm4rt1n91@icloud.com>
2025-07-29 16:15:27 -0500

    Day 1 practice - variables and input/output

---
+++ day1_variables.py
@@ -0,0 +1,5 @@
+name = input(f'What is your name?')
+color = input('fWhats your favorite color?')
+age = int(input(f'How old are you?'))
+
+print = (f'Hello {name}! Your favorite color is {color} and you are {age} years old.)
